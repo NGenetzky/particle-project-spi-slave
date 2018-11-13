70e6a26166cdba5896af74e539124260b6a19391
Nathan Genetzky <nathan@genetzky.us>
2018-11-13 00:59:14 -0600

    ci: Disable 'lib clean' and 'lib setup'

--- ci/travis.sh
+++ ci/travis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
 bash <(curl -sL https://raw.githubusercontent.com/nrobinson2000/po/master/ci/ci-install)
-po lib clean . -f &> /dev/null
-yes "no" | po lib setup # change to "yes" to prefer libraries from GitHub
+# po lib clean . -f &> /dev/null
+# yes "no" | po lib setup # change to "yes" to prefer libraries from GitHub
 po photon build
