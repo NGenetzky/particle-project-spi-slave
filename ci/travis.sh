262ecc833a66a70b62a589161ac051b2c5e89083
Nathan Genetzky <nathan@genetzky.us>
2018-11-12 22:10:10 -0600

    ci: Try apt-get update before the ci-install

--- ci/travis.sh
+++ ci/travis.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt-get update
 bash <(curl -sL https://raw.githubusercontent.com/nrobinson2000/po/master/ci/ci-install)
 po lib clean . -f &> /dev/null
 yes "no" | po lib setup # change to "yes" to prefer libraries from GitHub
