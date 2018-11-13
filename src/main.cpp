9d2830c17d7fc539e154fabb5c5aff840c436335
Nathan Genetzky <nathan@genetzky.us>
2018-11-12 22:29:55 -0600

    structure: Use 'particle project create'

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
-#include "Particle.h"
+/*
+ * Project particle-project-base
+ * Description:
+ * Author:
+ * Date:
+ */
 
-void setup() // Put setup code here to run once
-{
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
 
 }
 
-void loop() // Put code here to loop forever
-{
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
 
 }
